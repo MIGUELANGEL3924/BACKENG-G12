6555298a4dc63f269c1973fbe7bae52f8d7250b6
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-05-19 10:23:44 -0500

    integracion front y back

--- dia-1/1-intro-flask.py
+++ dia-1/1-intro-flask.py
@@ -25,54 +25,70 @@ productos = [{
 
 @app.route('/productos', methods=['GET', 'POST'])
 def gestionProductos():
-     # request > me da toda la informacion de mi cliente
+    # request > me da toda la informacion de mi cliente
     print(request.method)
     if request.method == 'GET':
+        productos_existentes = []
+        for producto in productos:
+            if producto is None:
+                # evita que el codigo de a continuacion no se ejecute por este ciclo
+                continue
+            productos_existentes.append(producto)
         return {
             'message': 'Los productos son',
-            'content': productos
+            'content': productos_existentes
         }
     elif request.method == 'POST':
-       print (request.json)
-       id = len(productos) + 1
-       data = request.json
-       # crea una nueva llave llamada id si no existe, pero si existe, reemplazara su valor
-       data['id'] = id
-       productos.append(data)
+        print(request.json)
+        id = len(productos) + 1
+        data = request.json
+        # crea una nueva llave llamada id si no existe, pero si existe, reemplazara su valor
+        data['id'] = id
+        productos.append(data)
 
-       return {
+        return {
             'message': 'Producto creado exitosamente'
         }
-    
 
-@app.route('/producto/<int:id>', methods=['GET'])
+
+@app.route('/producto/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def gestionProducto(id):
     print(id)
+    resultado = None
+    for producto in productos:
+        if producto is None:
+            continue
+        if producto['id'] == id:
+            resultado = producto
+            break
+
+    if resultado is None:
+        return {
+            'message': 'No se encontro el producto a buscar'
+        }
+
     if request.method == 'GET':
-        resultado = None
-        for producto in productos:
-            if producto['id'] == id:
-               resultado = producto
-               break
+        return {
+            'message': 'El producto es',
+            'content': resultado
+        }
 
-        if resultado is None:
-            return {
-                'message': 'No se encontro el producto a buscar'
-            }
+    elif request.method == 'PUT':
+        data = request.json
+        # la posicion sera el valor del id - 1 porque cuando la pos es 0 el id es 1 y asi sucesivamente
+        productos[id - 1] = data
+        # el primer corchete indicara la posicion de la lista y el segundo corchete indicara la propiedad del diccionario
+        productos[id - 1]['id'] = id
+        return {
+            'message': 'Producto actualizado exitosamente'
+        }
 
-        else:
-            return {
-                'message': 'El producto es',
-                'content': resultado
-            }
+    elif request.method == 'DELETE':
+        productos[id - 1] = None
 
-
-
-
-
-
-
-
+        return {
+            'message': 'Producto eliminado exitosamente'
+        }
 
 
 if __name__ == "__main__":
