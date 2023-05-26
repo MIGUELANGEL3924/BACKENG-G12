872ef4911c497fb2022913bc808bd1ff1bdc950c
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-05-26 12:46:19 -0500

    creando migraciones

--- dia-1/app.py
+++ dia-1/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+# "postgresql://username:password@localhost:5432/dbname"
+app.config['SQLALCHEMY_DATABASE_URI']
 
 
 @app.route('/')
---
+++ proyecto-mascotas/base_de_datos.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# conector entre mi bd y mi aplicacion de flask
+conexion = SQLAlchemy()
