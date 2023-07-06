c4e939e2ad6138249e5cd4023f00f4302b12c3a1
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-07-05 21:39:37 -0500

    creacion de proyecto

---
+++ src/base_de_datos.js
@@ -0,0 +1,5 @@
+import Prisma from "@prisma/client"
+
+// variable encargada de conectarse a la base de datos y por aca haremos todas las consultas
+export const conexion = new Prisma.PrismaClient()
+
