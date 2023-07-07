d200850504df2bbe5acdc47020ae1c7f390da79d
MIGUELANGEL3924 <mkitito2484@gmail.com>
2023-07-06 23:16:17 -0500

    jwt

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,5 +1,6 @@
 import * as UsuarioController from '../controllers/usuario.controller.js'
 import { Router } from 'express'
+import { validarToken } from '../utils/validador.js'
 
 // Utilizamos la interfaz de express del enrutador
 export const usuarioRouter = Router()
@@ -8,4 +9,6 @@ export const usuarioRouter = Router()
 // Creamos nuestra ruta del registro
 usuarioRouter.post('/registro', UsuarioController.registroUsuario)
 usuarioRouter.post('/login', UsuarioController.login)
+//porque si en el validarToken llega al next()pasara al controlador final(perfil)
+usuarioRouter.get('/perfil', validarToken, UsuarioController.perfil)
 
